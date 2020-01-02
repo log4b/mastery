44bccde83b8eb0f02a340bbbb34bbd16bef34d33
Bruno Silva <bruno.roberto.csilva@gmail.com>
2020-01-02 00:22:36 -0300

    (feat) Add validator layer. Also fix misstypes on boundary layer functions

--- boundary/lib/mastery/boundary/quiz_session.ex
+++ boundary/lib/mastery/boundary/quiz_session.ex
@@ -15,11 +15,12 @@ defmodule Mastery.Boundary.QuizSession do
   def handle_call({:answer_question, answer}, _from, {quiz, email}) do
     quiz
     |> Quiz.answer_question(Response.new(quiz, email, answer))
-    |> Quiz.select_question
+    |> Quiz.select_question()
     |> maybe_finish(email)
   end
 
   defp maybe_finish(nil, _email), do: {:stop, :normal, :finished, nil}
+
   defp maybe_finish(quiz, email) do
     {:reply, {quiz.current_question.asked, quiz.last_response.correct}, {quiz, email}}
   end
